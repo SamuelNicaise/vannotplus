966cfd76fe2737330d61c2d53acdbcf0a9000314
SamuelNicaise <ichtyornis@gmail.com>
2024-09-23 11:51:10 +0200

    Change config to YML, remove hardcoded paths, harmonize parsers

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 install:
 	pip install -e .
 
-run:
-	python -m vannotplus barcode -i /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/ped/trio_base.vcf -o /home1/HUB/bin/vannotplus/vannotplus/output.vcf -p /home1/data/WORK_DIR_SAM/Ped_raw/Data/ -a WES_AGILENT
+barcode:
+	python -m vannotplus barcode -i /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/ped/trio_base.vcf -o /home1/HUB/bin/vannotplus/vannotplus/output.vcf -c src/vannotplus/config.yml -a WES_AGILENT
+
+exomiser:
+	python -m vannotplus exomiser -i /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/ped/jb_trio_output.vcf -o /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/test/merged.vcf -c src/vannotplus/config.yml -a WES_AGILENT -v debug
+
+send:
+	rm -rf /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/vannotplus
+	cp -r /home1/HUB/bin/vannotplus/vannotplus /home1/L_PROD/NGS/BAS/HOWARD/data/nicaises
--- src/vannotplus/exomiser/exomiser.py
+++ src/vannotplus/exomiser/exomiser.py
@@ -1,29 +1,26 @@
 from datetime import datetime
 import json
+import logging as log
 import os
 from os.path import join as osj
 
 from cyvcf2 import cyvcf2
 import numpy as np
 
-from vannotplus.config import APP_TO_PED, MOUNT
-from vannotplus.family.barcode import run_shell
+from vannotplus.commons import load_ped, run_shell
 from vannotplus.family.ped9 import Ped
 
 TEMPLATE = osj(os.path.dirname(__file__), "template.json")
 
 
-def main_exomiser(input_vcf, output_dir, ped_dir, app):
+def main_exomiser(input_vcf, output_vcf, app, config):
     with open(TEMPLATE, "r") as f:
         template = json.load(f)
-    print(template)
-
-    ped_path = osj(ped_dir, APP_TO_PED[app])
-    ped = Ped(ped_file=ped_path)
-
-    vcf_in_container = input_vcf
+    output_dir = os.path.dirname(output_vcf)  # TODO: change output_dir to tmp dir
     output_dir_in_container = output_dir
-    for real_path, container_path in MOUNT.items():
+    ped = load_ped(config, app)
+    vcf_in_container = input_vcf
+    for real_path, container_path in config["mount"].items():
         if input_vcf.startswith(real_path):
             vcf_in_container = input_vcf.replace(real_path, container_path)
         if output_dir.startswith(real_path):
@@ -36,8 +33,6 @@ def main_exomiser(input_vcf, output_dir, ped_dir, app):
 
     sample_variant_dict = {}
     for s in vcf.samples:
-        print("s", s)
-
         # write monosample VCF
         sample_vcf = cyvcf2.VCF(input_vcf, samples=s)
         writer = cyvcf2.Writer(osj(output_dir, s + "_work.vcf"), sample_vcf)
@@ -57,23 +52,22 @@ def main_exomiser(input_vcf, output_dir, ped_dir, app):
         )
         template_file_in_container = osj(output_dir_in_container, s + "_template.json")
 
-        cmd = "-XX:ParallelGCThreads=12  -XX:MaxHeapSize=75G  -jar /root/howard/tools/exomiser/14.0.0/bin/exomiser-cli-14.0.0.jar"
+        cmd = f"-XX:ParallelGCThreads={config['exomiser']['threads']}  -XX:MaxHeapSize={config['exomiser']['heap']}  -jar {config['exomiser']['jar']}"
         cmd += f" --analysis={template_file_in_container}"
-        cmd += " --spring.config.location=/root/howard/databases/exomiser/sam/application.properties"
-        cmd += " --exomiser.data-directory=/root/howard/databases/exomiser/sam"
-        cmd = docker_cmd(cmd)
-        print(cmd)
-        # run_shell(cmd)
+        cmd += f" --spring.config.location={config['exomiser']['properties']}"
+        cmd += f" --exomiser.data-directory={config['exomiser']['db']}"
+        cmd = docker_cmd(config, cmd)
+        run_shell(cmd)
         sample_variant_dict[s] = get_annotated_variants(osj(output_dir, s + ".vcf.gz"))
 
-    annot_to_add = [
+    annots_to_add = [
         "EXOMISER_P_VALUE",
         "EXOMISER_GENE_COMBINED_SCORE",
         "EXOMISER_GENE_PHENO_SCORE",
         "EXOMISER_GENE_VARIANT_SCORE",
         "EXOMISER_VARIANT_SCORE",
     ]
-    for annot in annot_to_add:
+    for annot in annots_to_add:
         vcf.add_format_to_header(
             {
                 "ID": annot,
@@ -82,12 +76,12 @@ def main_exomiser(input_vcf, output_dir, ped_dir, app):
                 "Description": "Computed by vannotplus 1.0.0",
             }
         )
-    writer = cyvcf2.Writer(osj(output_dir, "merged.vcf"), vcf)
+    writer = cyvcf2.Writer(output_vcf, vcf)
     writer.write_header()
     for variant in vcf:
         key = "_".join([variant.CHROM, str(variant.POS), variant.REF, str(variant.ALT)])
 
-        for annot in annot_to_add:
+        for annot in annots_to_add:
             annot_list = []
             for s in vcf.samples:
                 try:
@@ -147,38 +141,11 @@ def write_template(
         json.dump(template, f)
 
 
-def docker_cmd(cmd):
-    docker_args = [
-        "docker",
-        "run",
-        "--rm",
-        "--name",
-        "sam_howard",
-        "--env",
-        f"http_proxy={os.environ['http_proxy']}",
-        "--env",
-        f"ftp_proxy={os.environ['ftp_proxy']}",
-        "--env",
-        f"https_proxy={os.environ['https_proxy']}",
-        "-v",
-        f"/home1/BAS/HOWARD/data:/data",
-        "-v",
-        f"/home1/BAS/HOWARD/databases:/databases",
-        "--entrypoint",
-        "/usr/bin/java",
-        "howard:0.11.0",
-    ]
+def docker_cmd(config, cmd):
+    docker_cmd = "docker run --rm --name vannotplus_exomiser"
+    for k, v in config["mount"].items():
+        docker_cmd += f" -v {k}:{v}"
+    docker_cmd += f" --entrypoint /usr/bin/java howard:{config['howard']['version']}"
 
-    cmd = " ".join(docker_args) + " " + cmd
+    cmd = docker_cmd + " " + cmd
     return cmd
-
-
-if __name__ == "__main__":
-    INPUT_VCF = "/home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/ped/jb_trio_output.vcf"
-    # PED_DIR = "/home1/data/WORK_DIR_SAM/Ped_raw/Data"
-    PED_DIR = "/home1/L_PROD/NGS/tmp/peds"
-    APP = "WES_AGILENT"
-    OUTPUT_DIR = "/home1/L_PROD/NGS/BAS/HOWARD/data/nicaises/test"
-    # OUTPUT_DIR = "/home1/BAS/HOWARD/data/nicaises/test"
-
-    main_exomiser(INPUT_VCF, OUTPUT_DIR, PED_DIR, APP)
