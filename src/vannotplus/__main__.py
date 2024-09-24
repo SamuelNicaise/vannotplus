5f32c9449aaeaa41b4c984ca64d2efbf2f32f149
SamuelNicaise <ichtyornis@gmail.com>
2024-09-24 19:32:34 +0200

    Add GMC

--- src/vannotplus/__main__.py
+++ src/vannotplus/__main__.py
@@ -11,7 +11,7 @@ import vannotplus
 from vannotplus.commons import set_log_level, load_config
 from vannotplus.family.barcode import main_barcode
 from vannotplus.exomiser.exomiser import main_exomiser
-from vannotplus.score import main_vannotscore
+from vannotplus.annot.score import main_annot
 
 
 def main():
@@ -26,21 +26,21 @@ def main():
 
     barcode_parser = subparsers.add_parser(
         "barcode",
-        help="",
+        help="Compute family barcode based on Ped file",
         formatter_class=argparse.MetavarTypeHelpFormatter,
     )
     barcode_parser.set_defaults(subparser="barcode")
 
     exomiser_parser = subparsers.add_parser(
         "exomiser",
-        help="",
+        help="Add exomiser scores for each sample in input VCF based on HPOs in Ped file",
         formatter_class=argparse.MetavarTypeHelpFormatter,
     )
     exomiser_parser.set_defaults(subparser="exomiser")
 
     score_parser = subparsers.add_parser(
-        "score",
-        help="",
+        "annot",
+        help="Add Gene Variations Count (GVC) for each sample and vannotscore for each variant to input VCF",
         formatter_class=argparse.MetavarTypeHelpFormatter,
     )
     score_parser.set_defaults(subparser="score")
@@ -69,6 +69,9 @@ def main():
             required=True,
             help="STARK application",
         )
+
+    # separate loops to keep args in order
+    for subparser in (barcode_parser, exomiser_parser, score_parser):
         subparser.add_argument(
             "-c",
             "--config",
@@ -76,8 +79,6 @@ def main():
             required=True,
             help="YAML config file",
         )
-
-    for subparser in (barcode_parser, exomiser_parser, score_parser):
         subparser.add_argument(
             "-v",
             "--verbosity",
@@ -92,16 +93,14 @@ def main():
     else:
         set_log_level(args.verbosity)
         log.debug(f"Args: {str(args)}")
+        config = load_config(args.config)
+        log.debug(f"config: {config}")
         if args.subparser == "barcode":
-            config = load_config(args.config)
-            log.debug(f"config: {config}")
             main_barcode(args.input, args.output, args.app, config)
         elif args.subparser == "exomiser":
-            config = load_config(args.config)
-            log.debug(f"config: {config}")
             main_exomiser(args.input, args.output, args.app, config)
         elif args.subparser == "score":
-            main_vannotscore(args.input, args.output)
+            main_annot(args.input, args.output, config)
 
 
 if __name__ == "__main__":
