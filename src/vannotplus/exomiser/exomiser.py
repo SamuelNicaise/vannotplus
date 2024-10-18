0375caaf1cfe3c1507bc18bde5df16a310580575
SamuelNicaise <ichtyornis@gmail.com>
2024-10-18 12:00:15 +0200

    Fix exomiser paths

--- src/vannotplus/exomiser/exomiser.py
+++ src/vannotplus/exomiser/exomiser.py
@@ -2,10 +2,13 @@ import json
 import logging as log
 import os
 from os.path import join as osj
+import tempfile
+
 
 from cyvcf2 import cyvcf2
 import numpy as np
 
+from vannotplus import __version__
 from vannotplus.commons import get_variant_id, load_ped, run_shell
 
 TEMPLATE = osj(os.path.dirname(__file__), "template.json")
@@ -14,15 +17,19 @@ TEMPLATE = osj(os.path.dirname(__file__), "template.json")
 def main_exomiser(input_vcf, output_vcf, app, config):
     with open(TEMPLATE, "r") as f:
         template = json.load(f)
-    output_dir = os.path.dirname(output_vcf)  # TODO: change output_dir to tmp dir
-    output_dir_in_container = output_dir
+    output_dir = os.path.dirname(output_vcf)
+    tmp_dir = tempfile.mkdtemp(dir=output_dir)
+    tmp_dir_in_container = tmp_dir
     ped = load_ped(config, app)
-    vcf_in_container = input_vcf
+    # vcf_in_container = input_vcf
     for real_path, container_path in config["mount"].items():
-        if input_vcf.startswith(real_path):
-            vcf_in_container = input_vcf.replace(real_path, container_path)
-        if output_dir.startswith(real_path):
-            output_dir_in_container = output_dir.replace(real_path, container_path)
+        # if input_vcf.startswith(real_path):
+        #   vcf_in_container = input_vcf.replace(real_path, container_path)
+        if tmp_dir.startswith(real_path):
+            tmp_dir_in_container = tmp_dir.replace(real_path, container_path)
+            print("tmp_dir_in_container", tmp_dir_in_container)
+    log.debug(f"tmp_dir: {tmp_dir}")
+    log.debug(f"tmp_dir_in_container: {tmp_dir_in_container}")
 
     vcf = cyvcf2.VCF(input_vcf)
     assembly = os.path.basename(vcf.get_header_type("reference")["reference"]).split(
@@ -33,7 +40,7 @@ def main_exomiser(input_vcf, output_vcf, app, config):
     for s in vcf.samples:
         # write monosample VCF
         sample_vcf = cyvcf2.VCF(input_vcf, samples=s)
-        writer = cyvcf2.Writer(osj(output_dir, s + "_work.vcf"), sample_vcf)
+        writer = cyvcf2.Writer(osj(tmp_dir, s + "_exomiserinput.vcf"), sample_vcf)
         writer.write_header()
         for variant in sample_vcf:
             writer.write_record(variant)
@@ -43,12 +50,12 @@ def main_exomiser(input_vcf, output_vcf, app, config):
             template,
             s,
             ped,
-            osj(output_dir_in_container, s + "_work.vcf"),
-            output_dir_in_container,
-            output_dir,
+            osj(tmp_dir_in_container, s + "_exomiserinput.vcf"),
+            tmp_dir_in_container,
+            tmp_dir,
             assembly,
         )
-        template_file_in_container = osj(output_dir_in_container, s + "_template.json")
+        template_file_in_container = osj(tmp_dir_in_container, s + "_template.json")
 
         cmd = f"-XX:ParallelGCThreads={config['exomiser']['threads']}  -XX:MaxHeapSize={config['exomiser']['heap']}  -jar {config['exomiser']['jar']}"
         cmd += f" --analysis={template_file_in_container}"
@@ -56,7 +63,7 @@ def main_exomiser(input_vcf, output_vcf, app, config):
         cmd += f" --exomiser.data-directory={config['exomiser']['db']}"
         cmd = docker_cmd(config, cmd)
         run_shell(cmd)
-        sample_variant_dict[s] = get_annotated_variants(osj(output_dir, s + ".vcf.gz"))
+        sample_variant_dict[s] = get_annotated_variants(osj(tmp_dir, s + ".vcf.gz"))
 
     annots_to_add = [
         "EXOMISER_P_VALUE",
@@ -71,7 +78,7 @@ def main_exomiser(input_vcf, output_vcf, app, config):
                 "ID": annot,
                 "Number": 1,
                 "Type": "Float",
-                "Description": "Computed by vannotplus 1.0.0",
+                "Description": f"Exported from vannotplus {__version__}",
             }
         )
     writer = cyvcf2.Writer(output_vcf, vcf)
@@ -93,6 +100,7 @@ def main_exomiser(input_vcf, output_vcf, app, config):
 
 
 def get_annotated_variants(vcf_path: str) -> dict:
+    log.debug(f"get_annotated_variants::vcf_path:{vcf_path}")
     vcf = cyvcf2.VCF(vcf_path)
 
     # verify description so indexes can be used safely later
@@ -102,7 +110,7 @@ def get_annotated_variants(vcf_path: str) -> dict:
         not in exomiser_header["Description"]
     ):
         raise ValueError(
-            f"Unexpected Exomiser description in VCF header: {exomiser_header}"
+            f"Unexpected Exomiser description in VCF header: {exomiser_header} -- failing VCF: {vcf_path}"
         )
 
     res = {}
@@ -120,7 +128,7 @@ def get_annotated_variants(vcf_path: str) -> dict:
 
 
 def write_template(
-    template, s, ped, vcf_in_container, output_dir_in_container, output_dir, assembly
+    template, s, ped, vcf_in_container, tmp_dir_in_container, tmp_dir, assembly
 ):
     template["phenopacket"]["subject"]["id"] = s
     if s in ped:
@@ -132,10 +140,10 @@ def write_template(
     template["phenopacket"]["htsFiles"] = [
         {"uri": vcf_in_container, "htsFormat": "VCF", "genomeAssembly": assembly}
     ]
-    template["outputOptions"]["outputDirectory"] = output_dir_in_container
+    template["outputOptions"]["outputDirectory"] = tmp_dir_in_container
     template["outputOptions"]["outputFileName"] = s
 
-    template_file = osj(output_dir, s + "_template.json")
+    template_file = osj(tmp_dir, s + "_template.json")
     with open(template_file, "w") as f:
         json.dump(template, f)
 
